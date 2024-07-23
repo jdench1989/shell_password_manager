cb057d05c3f72ce561308445a782eae6441ca6c5
jdench1989 <jdench1989@gmail.com>
2024-07-23 22:36:12 +0100

    Adds explanatory comments to case statement 1

--- password_manager.sh
+++ password_manager.sh
@@ -3,20 +3,26 @@
 #Prints welcome message
 echo
 echo "------------------------------"
-echo -e "Welcome to Password Manager!"
+echo "Welcome to Password Manager!"
 echo "------------------------------"
 echo
 
 #Options assigned to a variable for easier repeatability 
 options="echo -e 'Please select from the following options:\n1. Enter a new password\n2. View a saved password\n3. Delete a password\n4. Exit Password Manager'"
 
-#Evaluates user input and confirms choice to the user. Loop will only break on 1, 2, 3 and program will exit on 4.
+# Create vault.txt if it doesn't exist
+touch vault.txt
+
+# While loop presents available options and evaluates user input
+# User input 1, 2, or 3 will confirm the user's selection, carry out the appropriate action, then reset to option selection
+# User input 4 will exit the program
 while True; do
 	eval $options #Present available options
 	read -p "Enter your choice: " choice #Get input from user. 
-	choice_message="You have selected"
+	choice_message="You have selected" # Placeholder message stored in variable to be concatenated during case statement
 	case $choice in
         1) 
+		# Choice 1. Enter a new password. Requests user input for service and password. Generates a unique password id. Concatenates all three fields and appends to vault.txt as comma delimitted string
 		choice_message="$choice_message 'Enter a new password'"
 		echo $choice_message
 		read -p "Enter the name of the service for which to store a password: " service
