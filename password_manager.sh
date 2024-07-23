45c0f1da27df95eeb998b6bfa7871dbdf3f998b6
jdench1989 <jdench1989@gmail.com>
2024-07-23 16:44:42 +0100

    Implemented while loop for option selection stage in order to keep program running with invalid selection

--- password_manager.sh
+++ password_manager.sh
@@ -5,16 +5,26 @@ echo -e "Welcome to Password Manager!\nPlease select from the following options:
 echo "----------"
 echo -e "1. Enter a new password.\n2. View a saved password\n3. Delete a password\n4. Exit Password Manager"
 
-read choice
-
 choice_message=""
 
-case $choice in
-	1) choice_message="You selected 'Enter a new password'";;
-	2) choice_message="You selected 'View a saved password'";;
-	3) choice_message="You selected 'Delete a password'";;
-	4) choice_message="Goodbye!";;
-	*) echo "Invalid choice. Please try again"
-esac
+while True;
+do
+	read choice
+	case $choice in
+		1) choice_message="You selected 'Enter a new password'"
+		echo $choice_message
+		break;;
+		2) choice_message="You selected 'View a saved password'"
+		echo $choice_message
+		break;;
+		3) choice_message="You selected 'Delete a password'"
+		echo $choice_message
+		break;;
+		4) choice_message="Goodbye!"
+		echo $choice_message
+		break;;
+		*) echo "Invalid choice. Please try again"
+	esac
+done
 
-echo $choice_message
+
