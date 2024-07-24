d522287026a7888a89b3abc3577dd796104de489
jdench1989 <jdench1989@gmail.com>
2024-07-24 17:57:31 +0100

    Implements delete all fucntionality in addition to user confirmation before delete all and delete single entry

--- password_manager.sh
+++ password_manager.sh
@@ -8,7 +8,7 @@ echo "*****************************"
 echo
 
 #Options assigned to a variable for easier repeatability 
-options="echo -e 'Please select from the following options:\n1. Enter a new password or update an existing password\n2. View saved passwords\n3. Delete passwords\n4. Exit Password Manager'"
+options="echo -e 'Please select from the following options:\n1. Create or update an entry\n2. View entries\n3. Delete entry\n4. Exit Password Manager'"
 
 # Create vault.txt if it doesn't exist
 touch vault.txt
@@ -26,7 +26,7 @@ while True; do
 	case $choice in
         1) 
 		# Choice 1. Enter a new password. Requests user input for service name and password. Generates a unique password id. Concatenates all three fields and appends to vault.txt as comma delimitted string
-		choice_message="$choice_message 'Enter a new password or update an existing password'"
+		choice_message="$choice_message 'Create or update an entry'"
 		echo $choice_message
 		read -p "Enter the name of the service for which to store/update a password: " service
 		echo
@@ -65,47 +65,98 @@ while True; do
 
 		2) 
 		# Choice 2. Requests user input of a service name. Searches vault.txt column 2 for that service and then prints the associated password.
-		choice_message="$choice_message 'View a saved password'"
+		choice_message="$choice_message 'View entries'"
 		echo "$choice_message"
-		read -p "Enter the name of the service to be viewed: " service
+		echo -e "Please select from the following options:\n1. View all entries\n2. View a single entry"
+		read -p "Option: " choice
 		echo
-		# Retrieve both username and password in a single awk command
-		requested_credentials=$(awk -v serv="$service" '$2 == serv {print $0}' vault.txt)
-		requested_username=$(echo "$requested_credentials" | awk '{print $3}')
-		requested_password=$(echo "$requested_credentials" | awk '{print $4}')
-		# 'if' statement displays error if service is not recorded in vault.txt
-		if [ -n "$requested_username" ] && [ -n "$requested_password" ]; then
-			echo "The username for $service is: $requested_username"
-			echo "The password for $service is: $requested_password"
-			echo "------------------------------"
-			echo
-		else
-			echo "Service $service not found in the password vault."
-			echo "------------------------------"
-			echo
-		fi
+		while True; do
+			if [ "$choice" = "1" ]; then
+				echo -e "ID\tService\tUsername\tPassword\n$(cat vault.txt)" | column -t
+				echo "------------------------------"
+				echo
+				break
+			elif [ "$choice" = "2" ]; then
+				read -p "Enter the name of the service to be viewed: " service
+				echo
+				# Retrieve both username and password in a single awk command
+				requested_credentials=$(awk -v serv="$service" '$2 == serv {print $0}' vault.txt)
+				requested_username=$(echo "$requested_credentials" | awk '{print $3}')
+				requested_password=$(echo "$requested_credentials" | awk '{print $4}')
+				# 'if' statement displays error if service is not recorded in vault.txt
+				if [ -n "$requested_username" ] && [ -n "$requested_password" ]; then
+					echo "The username for $service is: $requested_username"
+					echo "The password for $service is: $requested_password"
+					echo "------------------------------"
+					echo
+					break
+				else
+					echo "Service $service not found in the password vault."
+					echo "------------------------------"
+					echo
+					break
+				fi
+			else
+				echo "Invalid selection"
+				echo "------------------------------"
+				echo
+			fi
+		done
 		;;
 
 		3) 
 		# Choice 3. Requests user input of service to be deleted. Searches vault.txt for service and removes line if there is a match. 
-		choice_message="$choice_message 'Delete a password'"
+		choice_message="$choice_message 'Delete entry'"
 		echo $choice_message
-		read -p "Enter the name of the service to be deleted: " service
+		echo -e "Please select from the following options:\n1. Delete a single entry\n2. Delete all entries"
+		read -p "Option: " choice
 		echo
-		requested_deletion_line=$(awk -v serv="$service" '$2 == serv {print NR}' vault.txt)
-		# 'if' statement displays error if service is not recorded in vault.txt
-		if [ -n "$requested_deletion_line" ]; then
-			# sed creates a copy of vault.txt called vault.txt.temp then replaces the original with the designated line removed. rm command deletes temp file after it is no longer needed
-			sed -i .temp "${requested_deletion_line}d" vault.txt ; rm vault.txt.temp 
-			echo "The password for $service has been deleted."
-			echo "------------------------------"
-			echo
-		else
-			echo "Service $service not found in the password vault."
-			echo "------------------------------"
-			echo
-		fi
-
+		while True; do
+			if [ "$choice" = "1" ]; then
+				read -p "Enter the name of the service to be deleted: " service
+				echo
+				requested_deletion_line=$(awk -v serv="$service" '$2 == serv {print NR}' vault.txt)
+				# 'if' statement displays error if service is not recorded in vault.txt
+				if [ -n "$requested_deletion_line" ]; then
+					read -p "The entry for $service will be permanently deleted. Are you sure? [y/n]: " confirm
+					if [ "$confirm" = "y" ]; then
+						# sed creates a copy of vault.txt called vault.txt.temp then replaces the original with the designated line removed. rm command deletes temp file after it is no longer needed
+						sed -i .temp "${requested_deletion_line}d" vault.txt ; rm vault.txt.temp 
+						echo "The password for $service has been deleted."
+						echo "------------------------------"
+						echo
+						break
+					else
+						echo "Delete action cancelled"
+						echo "------------------------------"
+						echo
+						break
+					fi
+				else
+					echo "Service $service not found in the password vault."
+					echo "------------------------------"
+					echo
+					break
+				fi
+			elif [ "$choice" = "2" ]; then
+				read -p "All entries will be permanently deleted. Are you sure? [y/n]: " confirm
+				echo
+				if [ "$confirm" = "y" ]; then
+					$(> vault.txt)
+					echo "All entries deleted succesfully"
+					break
+				else
+					echo "Delete action cancelled"
+					echo "------------------------------"
+					echo
+					break
+				fi
+			else
+				echo "Invalid selection"
+				echo "------------------------------"
+				echo
+			fi
+		done
 		;;
 
 		4) 
